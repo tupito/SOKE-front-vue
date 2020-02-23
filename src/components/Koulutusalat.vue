4ff8a3d4e1d0eeb1a9749654899b3027df1d0812
TT <tomi.tupiini@gmail.com>
2020-02-23 15:31:20 +0200

    edit koulutusalat, pass data with routerlink

--- src/components/Koulutusalat.vue
+++ src/components/Koulutusalat.vue
@@ -5,7 +5,7 @@
       <router-link
         :to="{
           name: 'Realizations',
-          params: { id: item.routeUrl, name: item.name }
+          params: { id: item.routeUrl, name: item.name, courses: item.courses } // Välitetään router-linkillä data, esim. koulutusalan tarjolla olevat toteutukset
         }"
       >{{ item.name }}</router-link>|
       <router-link to="/realizations">testeri</router-link>
@@ -17,7 +17,28 @@
 const koulutusalatDummy = [
   {
     name: "Kauppa, hallinto ja oikeustieteet",
-    routeUrl: "kauppa-hallinto-ja-oikeustieteet"
+    routeUrl: "kauppa-hallinto-ja-oikeustieteet",
+    courses: [
+      {
+        courseName:
+          "Yritystoiminta ja yrittäjyys, 3 op, 26.08.2019 - 31.05.2020"
+      },
+      {
+        courseName:
+          "Tutkimus- ja kehittämistyön perusteet, 2 op, 26.08.2019 - 31.05.2020"
+      }
+    ]
+  },
+  {
+    name: "Tekniikan alat",
+    routeUrl: "tekniikan-alat",
+    courses: [
+      {
+        courseName:
+          "Sähköturvallisuussäädökset ja -standardit, 3 op, 12.08.2019 - 31.05.2020"
+      },
+      { courseName: "Sähkö- ja lämpöoppi, 3 op, 12.08.2019 - 10.05.2020" }
+    ]
   }
   /*   "Maa- ja metsätalousalat",
   "Palvelualat",
