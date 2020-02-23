6ba4018baa9c8b3b723ce6cbcfab0aa2315df69c
TT <tomi.tupiini@gmail.com>
2020-02-23 09:56:34 +0200

    edit generated home -> vueinfo

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import VueInfo from "../views/VueInfo.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "/vueinfo",
+    name: "VueInfo",
+    component: VueInfo
   },
   {
     path: "/about",
