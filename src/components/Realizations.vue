14600d0fb71d04379cf5b0423f5fad54b1b873cb
Tupiini <Tomi.P.Tupiini@seamk.fi>
2020-02-25 17:25:20 +0200

    edit realizations.vue, add sorting

--- src/components/Realizations.vue
+++ src/components/Realizations.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <h2>Koulutusalan {{ $route.params.name }} avoimen AMK:n opintojaksot</h2>
-    <!-- <div v-for="item in courses" :key="item.id"> -->
-    <div v-for="item in realizationsWithFinnishDates" :key="item.id">
+    <div>
+      <button v-on:click="filterRealizations()">todo filterRealizations</button>
+      <button
+        v-on:click="
+          changeParam('enrollmentEnd');
+          reverseSort = !reverseSort;
+        "
+      >Järjestä ilmoittautumisen päättymisen mukaan</button>
+      <button
+        v-on:click="
+          changeParam('localizedNameFi');
+          reverseSort = !reverseSort;
+        "
+      >Järjestä nimen mukaan</button>
+    </div>
+
+    <!-- <div v-for="item in realizationsWithFinnishDates" :key="item.id"> -->
+    <div v-for="item in sortedRealizations" :key="item.id">
       <router-link
         :to="{
           name: 'RealizationItem',
           params: { id: item.id, realization: item }
         }"
       >
-        {{ item.courseUnit.localizedName.valueFi }}
+        {{ item.localizedNameFi }}
         ( {{ item.startDate }} - {{ item.endDate }} ) ( ilmoittautuminen
         päättyy: {{ item.enrollmentEnd }})
       </router-link>
@@ -19,7 +35,7 @@
 
 <script>
 import { exampleMixin } from "@/mixins/exampleMixin.js";
-import dateLib from "@/lib/customFunctions.js";
+//import lib from "@/lib/customFunctions.js";
 
 export default {
   name: "Realizations-component",
@@ -27,24 +43,58 @@ export default {
   created() {
     console.log("Realizations-component called");
   },
+  beforeMount() {
+    // simplify nested data for sorting
+    this.simplifyNestedData();
+  },
   data() {
     return {
       // $route.params = haetaan välitettyjä tietoja router-link:ltä
-      courses: this.$route.params.realizations
+      realizations: this.$route.params.realizations,
+      sortParam: "",
+      reverseSort: false
     };
   },
   // Computed properties are for transforming data for the presentation layer, not to alter or change data!
   computed: {
     // Näytetään toteutusten päivämäärät suomalaisessa muodossa (2020-05-31T23:59:00 -> 31.5.2020)
-    realizationsWithFinnishDates() {
-      let realizations = this.$route.params.realizations;
-      realizations.forEach(elem => {
-        elem.startDate = dateLib.toFinDate(elem.startDate);
-        elem.endDate = dateLib.toFinDate(elem.endDate);
-        elem.enrollmentStart = dateLib.toFinDate(elem.enrollmentStart);
-        elem.enrollmentEnd = dateLib.toFinDate(elem.enrollmentEnd);
+    /*     realizationsWithFinnishDates() {
+      console.log("computed");
+      let realizationFinDates = this.realizations;
+      realizationFinDates.forEach(elem => {
+        elem.startDate = lib.toFinDate(elem.startDate);
+        elem.endDate = lib.toFinDate(elem.endDate);
+        elem.enrollmentStart = lib.toFinDate(elem.enrollmentStart);
+        elem.enrollmentEnd = lib.toFinDate(elem.enrollmentEnd);
       });
-      return realizations;
+      return realizationFinDates;
+    }, */
+    sortedRealizations() {
+      console.log(this.sortParam, this.reverseSort);
+      if (this.sortParam) {
+        return this.realizations.slice().sort((a, b) => {
+          if (this.reverseSort) {
+            return b[this.sortParam].localeCompare(a[this.sortParam]);
+          } else {
+            return a[this.sortParam].localeCompare(b[this.sortParam]);
+          }
+        });
+      } else {
+        return this.realizations;
+      }
+    }
+  },
+  methods: {
+    simplifyNestedData() {
+      this.realizations.forEach(elem => {
+        elem.localizedNameFi = elem.courseUnit.localizedName.valueFi;
+      });
+    },
+    filterRealizations() {
+      console.log("filterRealizations");
+    },
+    changeParam(param) {
+      this.sortParam = param;
     }
   }
 };
